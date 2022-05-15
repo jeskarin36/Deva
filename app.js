3a375aad2cba46ab2d43ff574181f753264421c2
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2022-05-15 10:15:17 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http= require('http');
-const sever= http.createServer((req,res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hola mundo');
-    res.end();
-},3000);
-
-
